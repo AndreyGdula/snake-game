23ec4d0c151a91e8ce1a126b1f45b0bbadc557a6
AndreyGdula <andreygdula@gmail.com>
2024-04-02 22:01:04 -0300

    complete

--- script/script.js
+++ script/script.js
@@ -5,7 +5,7 @@ const menuGameover = document.querySelector("div.menu-gameover")
 const finalScore = document.querySelector("p.final-score")
 const buttonStart = document.querySelector("button.btn-start")
 
-var score = 1
+const speedGame = 150
 const size = 30
 const sound = new Audio("../files/score.mp3")
 
@@ -33,13 +33,16 @@ let direction, loopId
 
 const drawSnake = () => {
     ctx.fillStyle = "white"
+    ctx.shadowColor = "white"
     
     snake.forEach((position, index) => {
+        ctx.shadowBlur = 8
         if (index == snake.length - 1) {
             ctx.fillStyle = "lightgray"
         }
 
         ctx.fillRect(position.x, position.y, size, size)
+        ctx.shadowBlur = 0
     })
 }
 
@@ -63,6 +66,8 @@ const moveSnake = () => {
 
 const drawFood = () => {
     ctx.fillStyle = food.color
+    ctx.shadowColor = food.color
+    ctx.shadowBlur = 15
     ctx.fillRect(food.x, food.y, size, size)
 }
 
@@ -76,7 +81,7 @@ const eatFood = () => {
         food.x = randomPosition()
         food.y = randomPosition()
 
-        h1.innerHTML = `SCORE: ${score++}`
+        h1.innerHTML = `SCORE: ${snake.length - 2}`
 
         while (snake.find((position) => position.x == food.x && position.y == food.y)) {
             food.x = randomPosition()
@@ -87,7 +92,7 @@ const eatFood = () => {
 
 const collision = () => {
     const head = snake[snake.length - 1]
-    const neckIndex = snake[snake.length - 2]
+    const neckIndex = snake.length - 2
 
     const wallCollision = head.x < 0 || head.x >= canvas.width || head.y < 0 || head.y >= canvas.width
 
@@ -98,13 +103,6 @@ const collision = () => {
     if (wallCollision || snakeCollision) {
         gameover()
     }
-}
-
-const gameover = () => {
-    direction = undefined
-    canvas.style.filter = "blur(5px)"
-    menuGameover.style.display = "flex"
-    finalScore.innerHTML = `SCORE: ${score - 1}`
 }
 
 const drawGrid = () => {
@@ -124,6 +122,13 @@ const drawGrid = () => {
     }
 }
 
+const gameover = () => {
+    direction = undefined
+    canvas.style.filter = "blur(5px)"
+    menuGameover.style.display = "flex"
+    finalScore.innerHTML = `SCORE: ${snake.length - 2}`
+}
+
 const gameLoop = () => {
     clearInterval(loopId)
     ctx.clearRect(0, 0, 600, 600)
@@ -137,7 +142,7 @@ const gameLoop = () => {
 
     loopId = setInterval(() => {
         gameLoop()
-    }, 300)
+    }, speedGame)
 }
 
 document.addEventListener('keydown', ({key}) => {
@@ -152,10 +157,7 @@ document.addEventListener('keydown', ({key}) => {
     }
 })
 
-gameLoop()
-
 buttonStart.addEventListener('click', () => {
-    score = 0
     menuGameover.style.display = "none"
     canvas.style.filter = "blur(0px)"
     h1.innerHTML = `SCORE: 0`
@@ -164,3 +166,5 @@ buttonStart.addEventListener('click', () => {
         {x: 330, y: 300}
     ]
 })
+
+gameLoop()
